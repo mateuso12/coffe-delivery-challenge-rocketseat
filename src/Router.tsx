6dc979c4c98bd1378f3c0a7ccde949df0295c083
mateuso12 <mateuso12@hotmail.com.br>
2022-10-25 21:04:08 -0300

    feat: adding header to all pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layouts/DefaultLayout'
+import { Checkout } from './pages/Checkout/index'
 import { Homepage } from './pages/Homepage/index'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />}></Route>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
     </Routes>
   )
 }
