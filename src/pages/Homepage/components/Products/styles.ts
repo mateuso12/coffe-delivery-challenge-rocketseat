510425eed18ef0181b9e77b53ba0e77bfa3613b6
mateuso12 <mateuso12@hotmail.com.br>
2022-11-21 22:46:31 -0300

    refactor: new styles from products

--- src/pages/Homepage/components/Products/styles.ts
+++ src/pages/Homepage/components/Products/styles.ts
@@ -2,18 +2,30 @@ import styled from 'styled-components'
 
 export const MenuTitle = styled.h1`
   margin-bottom: 54px;
+
+  padding: 0 10rem;
+
+  @media only screen and (max-width: 1007px) {
+    padding: 0 2.75rem;
+  }
+  @media only screen and (max-width: 640px) {
+    padding: 0 1.2rem;
+  }
 `
 
 export const CoffeeGrid = styled.div`
-  display: -webkit-flex;
-  display: -moz-flex;
-  display: flex;
-  -webkit-flex-flow: row wrap;
-  -moz-flex-flow: row wrap;
-  flex-flow: row wrap;
-  -webkit-justify-content: center;
-  -moz-justify-content: center;
-  justify-content: center;
-  gap: 2rem;
-  row-gap: 3.5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  max-width: 100vw;
+  column-gap: 2rem;
+  row-gap: 2.5rem;
+
+  padding: 0 10rem;
+
+  @media only screen and (max-width: 1007px) {
+    padding: 0 2.75rem;
+  }
+  @media only screen and (max-width: 640px) {
+    padding: 0 1.2rem;
+  }
 `
