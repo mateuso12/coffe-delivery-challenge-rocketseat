7f979460aa2d834a025c98155d6a339a8fe1f67f
mateuso12 <mateuso12@hotmail.com.br>
2022-11-21 22:50:32 -0300

    feat: update in a header component and homepage

--- src/pages/Homepage/components/Header/index.tsx
+++ src/pages/Homepage/components/Header/index.tsx
@@ -1,22 +1,34 @@
-import { MapPin, ShoppingCart } from 'phosphor-react'
+import { MapPin } from 'phosphor-react'
 import { HeaderContainer, NavbarItems } from './styles'
 import logo from '../../../../assets/logo.svg'
+import { CartButton } from '../../../../components/CartButton'
+import { useNavigate } from 'react-router-dom'
 
 export function Header() {
+  const navigate = useNavigate()
+
+  function handleClickGoToHome() {
+    return navigate('/')
+  }
+
   return (
     <HeaderContainer>
       <img
         src={logo}
         alt="Copo de café com foguete e logotipo da empresa Coffe Delivery"
+        onClick={handleClickGoToHome}
       />
       <NavbarItems>
         <div>
           <MapPin weight="fill" height={22} width={22} />
           <p>Jaguariúna - SP</p>
         </div>
-        <button>
-          <ShoppingCart weight="fill" height={22} width={22} />
-        </button>
+        <CartButton
+          counter={23}
+          bgColor={'brand-yellow'}
+          fillColor={'brand-yellow-dark'}
+          disabled={false}
+        />
       </NavbarItems>
     </HeaderContainer>
   )
--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
 
 export const ContentContainer = styled.div`
-  overflow: hidden;
+  overflow: auto;
 `
